7c47b29e257b443b458a1679d1ae77b8ce4218fb
musokestephen962-source <musokestephen962@gmail.com>
2026-09-18 02:48:18 -0400

    Add D1 test API endpoint

--- worker.js
+++ worker.js
@@ -1,5 +1,22 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/api/test-db") {
+      const result = await env.DB
+        .prepare("SELECT name FROM sqlite_master WHERE type='table'")
+        .all();
+
+      return new Response(
+        JSON.stringify(result),
+        {
+          headers: {
+            "content-type": "application/json"
+          }
+        }
+      );
+    }
+
     return env.ASSETS.fetch(request);
   }
 };
